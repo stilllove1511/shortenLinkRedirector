efeda921a41ab71e910a176b47b00bab3179102d
Your Name <commitment1511@outlook.com>
2023-02-28 23:04:33 +0700

    modify env

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,16 +1,15 @@
 import { createClient } from "redis";
 
 const redis = createClient({
-    host: process.env.REDIS_HOST||'localhost',
-    port: process.env.REDIS_PORT||'6379'
+    url: process.env.REDIS_URL,
 });
 
 export const connectRedis = async () => {
     try {
         await redis.connect();
     } catch (error) {
-        // console.log(error);
-        console.log("connect redis fail");
+        console.log(error);
+        // console.log("connect redis fail");
     }
 };
 
